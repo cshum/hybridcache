38be84bbba2e957b6b13445bdf8a4ab48f3e812b
Adrian Shum <adrian@cshum.com>
2021-11-07 22:18:37 +0800

    func_test add non expire case

--- func_test.go
+++ func_test.go
@@ -2,7 +2,6 @@ package cache
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 	"time"
 )
@@ -11,29 +10,34 @@ func TestFunc(t *testing.T) {
 	c := NewMemory(10, int64(10<<20), -1)
 	if val, err := Func(c, "a", func() ([]byte, error) {
 		return []byte("b"), nil
-	}, time.Nanosecond, time.Minute); err != nil || string(val) != "b" {
-		fmt.Println(string(val))
-		t.Error(err)
+	}, time.Millisecond, time.Minute); err != nil || string(val) != "b" {
+		t.Error(string(val), err)
 	}
-	time.Sleep(time.Millisecond)
+	time.Sleep(time.Millisecond * 2)
 	if val, err := Func(c, "a", func() ([]byte, error) {
 		return nil, errors.New("should absorb error")
-	}, time.Nanosecond, time.Minute); err != nil || string(val) != "b" {
-		fmt.Println(string(val))
-		t.Error(err)
+	}, time.Millisecond, time.Minute); err != nil || string(val) != "b" {
+		t.Error(string(val), err)
 	}
-	time.Sleep(time.Millisecond)
+	time.Sleep(time.Millisecond * 2)
 	if val, err := Func(c, "a", func() ([]byte, error) {
 		return []byte("c"), nil
-	}, time.Nanosecond, time.Minute); err != nil || string(val) != "b" {
-		fmt.Println(string(val))
-		t.Error(err)
+	}, time.Millisecond, time.Minute); err != nil || string(val) != "b" {
+		t.Error(string(val), err)
 	}
-	time.Sleep(time.Millisecond)
+	time.Sleep(time.Millisecond * 2)
 	if val, err := Func(c, "a", func() ([]byte, error) {
-		return nil, errors.New("should absorb error")
-	}, time.Nanosecond, time.Minute); err != nil || string(val) != "c" {
-		fmt.Println(string(val))
-		t.Error(err)
+		return []byte("d"), nil
+	}, time.Millisecond*10, time.Minute); err != nil || string(val) != "c" {
+		t.Error(string(val), err)
+	}
+	time.Sleep(time.Millisecond * 2)
+	for i := 0; i < 3; i++ {
+		if val, err := Func(c, "a", func() ([]byte, error) {
+			return []byte("e"), nil
+		}, time.Millisecond, time.Minute); err != nil || string(val) != "d" {
+			t.Error(string(val), err)
+		}
+		time.Sleep(time.Millisecond)
 	}
 }
