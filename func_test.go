c8b2d436e0d4ec1160e7eb75fa86cc151ebe1790
Adrian Shum <adrian.shum@goxip.com>
2021-11-12 18:41:14 +0800

    refactor TestFunc cases

--- func_test.go
+++ func_test.go
@@ -11,138 +11,161 @@ import (
 	"time"
 )
 
-func TestFuncDoBytes(t *testing.T) {
-	var (
-		c    = NewMemory(10, int64(10<<20), -1)
-		fn1  = NewFunc(c, time.Millisecond, time.Millisecond, time.Minute)
-		fn1j = NewFunc(c, time.Millisecond, time.Millisecond, time.Minute)
+func TestFunc(t *testing.T) {
+	DoTestFuncDo("Memory", t, NewMemory(10, int64(10<<20), -1))
+	DoTestFuncDo("Redis", t, createRedisCache(4))
+	DoTestFuncDo("Hybrid", t, NewHybrid(
+		createRedisCache(5),
+		NewMemory(10, int64(10<<20), -1),
+	))
+	DoTestFuncDo("HybridRedis", t, NewHybrid(
+		createRedisCache(6),
+		NewMemory(10, int64(10<<20), time.Nanosecond)),
 	)
-	fn1j.Marshal = json.Marshal
-	fn1j.Unmarshal = json.Unmarshal
-	tests := []struct {
-		name         string
-		key          string
-		c            *Func
-		fn           func(ctx context.Context) ([]byte, error)
-		wantVal      []byte
-		wantErr      string
-		wantErrExact error
-		noErr        bool
-		repeat       int
-		sleep        time.Duration
-	}{
-		{
-			name: "should be cache miss",
-			key:  "a",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				return []byte("b"), nil
+	DoTestFuncDoBytes("Memory", t, NewMemory(10, int64(10<<20), -1))
+	DoTestFuncDoBytes("Redis", t, createRedisCache(7))
+	DoTestFuncDoBytes("Hybrid", t, NewHybrid(
+		createRedisCache(8),
+		NewMemory(10, int64(10<<20), -1),
+	))
+	DoTestFuncDo("HybridRedis", t, NewHybrid(
+		createRedisCache(9),
+		NewMemory(10, int64(10<<20), time.Nanosecond)),
+	)
+}
+
+func DoTestFuncDoBytes(name string, t *testing.T, c Cache) {
+	t.Run(name+"FuncDo", func(t *testing.T) {
+		var (
+			fn1  = NewFunc(c, time.Millisecond*50, time.Millisecond*50, time.Second*2)
+			fn1j = NewFunc(c, time.Millisecond*50, time.Millisecond*50, time.Second*2)
+		)
+		fn1j.Marshal = json.Marshal
+		fn1j.Unmarshal = json.Unmarshal
+		tests := []struct {
+			name         string
+			key          string
+			c            *Func
+			fn           func(ctx context.Context) ([]byte, error)
+			wantVal      []byte
+			wantErr      string
+			wantErrExact error
+			noErr        bool
+			repeat       int
+			sleep        time.Duration
+		}{
+			{
+				name: "should be cache miss",
+				key:  "a",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					return []byte("b"), nil
+				},
+				wantVal: []byte("b"),
+				noErr:   true,
+				sleep:   time.Millisecond,
 			},
-			wantVal: []byte("b"),
-			noErr:   true,
-			sleep:   time.Millisecond,
-		},
-		{
-			name: "should absorb error",
-			key:  "a",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				return nil, errors.New("booommmm")
+			{
+				name: "should absorb error",
+				key:  "a",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					return nil, errors.New("booommmm")
+				},
+				wantVal: []byte("b"),
+				noErr:   true,
+				sleep:   time.Millisecond * 101,
 			},
-			wantVal: []byte("b"),
-			noErr:   true,
-			sleep:   time.Millisecond * 101,
-		},
-		{
-			name: "should use cache",
-			key:  "a",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				return []byte("c"), nil
+			{
+				name: "should use cache",
+				key:  "a",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					return []byte("c"), nil
+				},
+				wantVal: []byte("b"),
+				noErr:   true,
+				sleep:   time.Millisecond * 101,
 			},
-			wantVal: []byte("b"),
-			noErr:   true,
-			sleep:   time.Millisecond * 101,
-		},
-		{
-			name: "should need refresh",
-			key:  "a",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				return []byte("d"), nil
+			{
+				name: "should need refresh",
+				key:  "a",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					return []byte("d"), nil
+				},
+				wantVal: []byte("c"),
+				noErr:   true,
+				sleep:   time.Millisecond * 2,
 			},
-			wantVal: []byte("c"),
-			noErr:   true,
-			sleep:   time.Millisecond * 2,
-		},
-		{
-			name: "should return expected error",
-			key:  "b",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				return nil, errors.New("expected error")
+			{
+				name: "should return expected error",
+				key:  "b",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					return nil, errors.New("expected error")
+				},
+				wantVal: nil,
+				noErr:   false,
+				wantErr: "expected error",
+				sleep:   time.Millisecond,
 			},
-			wantVal: nil,
-			noErr:   false,
-			wantErr: "expected error",
-			sleep:   time.Millisecond,
-		},
-		{
-			name: "ErrNoCache handling",
-			key:  "c",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				if IsDetached(ctx) {
-					t.Error("ErrNoCache should not detach")
-				}
-				return []byte("c1"), ErrNoCache
+			{
+				name: "ErrNoCache handling",
+				key:  "c",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					if IsDetached(ctx) {
+						t.Error("ErrNoCache should not detach")
+					}
+					return []byte("c1"), ErrNoCache
+				},
+				wantVal: []byte("c1"),
+				noErr:   true,
+				sleep:   time.Millisecond * 100,
 			},
-			wantVal: []byte("c1"),
-			noErr:   true,
-			sleep:   time.Millisecond * 100,
-		},
-		{
-			name: "ErrNoCache handling",
-			key:  "c",
-			c:    fn1,
-			fn: func(ctx context.Context) ([]byte, error) {
-				if IsDetached(ctx) {
-					t.Error("ErrNoCache should not detach")
-				}
-				return []byte("c2"), ErrNoCache
+			{
+				name: "ErrNoCache handling",
+				key:  "c",
+				c:    fn1,
+				fn: func(ctx context.Context) ([]byte, error) {
+					if IsDetached(ctx) {
+						t.Error("ErrNoCache should not detach")
+					}
+					return []byte("c2"), ErrNoCache
+				},
+				wantVal: []byte("c2"),
+				noErr:   true,
+				sleep:   time.Millisecond,
 			},
-			wantVal: []byte("c2"),
-			noErr:   true,
-			sleep:   time.Millisecond,
-		},
-	}
-	for _, tt := range tests {
-		for i := 0; i <= tt.repeat; i++ {
-			t.Run(tt.name, func(t *testing.T) {
-				ctx := context.Background()
-				val, err := tt.c.DoBytes(ctx, tt.key, tt.fn)
-				if tt.noErr && err != nil {
-					t.Error(err)
-				}
-				if !reflect.DeepEqual(val, tt.wantVal) {
-					t.Errorf(" = %v, want %v", string(val), string(tt.wantVal))
-				}
-				if tt.wantErr != "" && (err == nil || err.Error() != tt.wantErr) {
-					t.Errorf(" = %v, want %v", err, tt.wantErr)
-				}
-				if tt.wantErrExact != nil && err != tt.wantErrExact {
-					t.Errorf(" = %v, want %v", err, tt.wantErrExact)
-				}
-				time.Sleep(tt.sleep)
-			})
 		}
-	}
+		for _, tt := range tests {
+			for i := 0; i <= tt.repeat; i++ {
+				t.Run(tt.name, func(t *testing.T) {
+					ctx := context.Background()
+					val, err := tt.c.DoBytes(ctx, tt.key, tt.fn)
+					if tt.noErr && err != nil {
+						t.Error(err)
+					}
+					if !reflect.DeepEqual(val, tt.wantVal) {
+						t.Errorf(" = %v, want %v", string(val), string(tt.wantVal))
+					}
+					if tt.wantErr != "" && (err == nil || err.Error() != tt.wantErr) {
+						t.Errorf(" = %v, want %v", err, tt.wantErr)
+					}
+					if tt.wantErrExact != nil && err != tt.wantErrExact {
+						t.Errorf(" = %v, want %v", err, tt.wantErrExact)
+					}
+					time.Sleep(tt.sleep)
+				})
+			}
+		}
+	})
 }
 
 func DoTestFuncDo(name string, t *testing.T, c Cache) {
 	t.Run(name+"FuncDo", func(t *testing.T) {
 		var (
-			//c    = NewMemory(10, int64(10<<20), -1)
 			fn1  = NewFunc(c, time.Millisecond*50, time.Millisecond*50, time.Second*2)
 			fn1j = NewFunc(c, time.Millisecond*50, time.Millisecond*50, time.Second*2)
 		)
@@ -301,21 +324,7 @@ func DoTestFuncDo(name string, t *testing.T, c Cache) {
 				})
 			}
 		}
-
 	})
-}
-
-func TestFunc_Do(t *testing.T) {
-	DoTestFuncDo("Memory", t, NewMemory(10, int64(10<<20), -1))
-	DoTestFuncDo("Redis", t, createRedisCache(4))
-	DoTestFuncDo("Hybrid", t, NewHybrid(
-		createRedisCache(5),
-		NewMemory(10, int64(10<<20), -1),
-	))
-	DoTestFuncDo("HybridRedis", t, NewHybrid(
-		createRedisCache(6),
-		NewMemory(10, int64(10<<20), time.Nanosecond)),
-	)
 }
 
 func TestFunc_Do_Concurrent(t *testing.T) {
